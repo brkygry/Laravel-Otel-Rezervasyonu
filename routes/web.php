346aa6e3b3f87cec296af5a659f69b0fddb2ebcd
brkygry <48410920+brkygry@users.noreply.github.com>
2021-01-07 01:29:13 +0300

    Admin Panele Login yapılabiliyor ve Logout butonu çalışır hale getirildi.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home2', function () {
-    return view('welcome');
-});
-
 Route::redirect('/anasayfa', '/');
 Route::redirect('/home', '/');
 
@@ -25,8 +21,35 @@ Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
 
 //Admin
+Route::get('/admin',[\App\Http\Controllers\Admin\HomeController::class, 'index']) -> name('admin_home')->middleware("auth");
 
-Route::get('/admin',[\App\Http\Controllers\Admin\HomeController::class, 'index']) -> name('adminhome');
+
+//Admin Login
+Route::get('/admin/login', [\App\Http\Controllers\HomeController::class, 'login'])->name('admin_login');
+Route::post('/admin/logincheck', [\App\Http\Controllers\HomeController::class, 'logincheck'])->name('admin_login_check');
+Route::get('/admin/logout', [\App\Http\Controllers\HomeController::class, 'logout'])->name('admin_log_out');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
